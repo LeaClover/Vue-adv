f3b2b2fe04440fd5f6efa215ee365cfdcd1574f3
LeaClover <plesen_14@mail.ru>
2023-11-04 18:22:56 +0300

    add new project hw5

---
+++ homework5/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <ProductDetails v-for="product in products" :product="product" :key="product.id" />
+  </div>
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetails
+  },
+  data () {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'product1',
+          price: 200,
+          available: true
+        },
+        {
+          id: 2,
+          name: 'product2',
+          price: 300,
+          available: true
+        },
+        {
+          id: 3,
+          name: 'product3',
+          price: 500,
+          available: false
+        },
+        {
+          id: 4,
+          name: 'product4',
+          price: 300,
+          available: true
+        },
+        {
+          id: 5,
+          name: 'product5',
+          price: 400,
+          available: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
